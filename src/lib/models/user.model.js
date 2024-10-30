428dec18abca8928bb17eebc26350bfb06dd4b25
MustafaTahirAkdemir <themtahir@gmail.com>
2024-10-30 05:03:20 +0300

    Add mongoDb and mongoose to save user data from Clerk

---
+++ src/lib/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+
+import mongoose from 'mongoose';
+const userSchema = new mongoose.Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      
+    },
+    lastName: {
+      type: String,
+      
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+export default User;
